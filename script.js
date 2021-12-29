aaebb6c62740d82bee6a8f372f095098846426f3
Massaru <massaruhagiopro@gmail.com>
2021-12-29 15:50:50 +0900

    modificando teclas

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("testando");
+var numero = document.getElementById("calculo");
+console.log(numero.textContent)
+function number(num) {
+    console.log(numero.innerText += num.innerText);
+    // console.log(numero.innerText = numero.innerText + num);
+}
+function calculo() {
+    let teste = JSON.parse(numero.innerText);
+    console.log(teste)
+}
+function soma(){
+    
+}
