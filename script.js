dd4200448ddde617392af6db3406d7a244f27188
Massaru <massaruhagiopro@gmail.com>
2021-12-30 01:39:36 +0900

    terminando logica de teclas

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 var numero = document.getElementById("calculo");
 console.log(numero.textContent);
-function calculo(num) {
+function logicaDeTeclas(num) {
     let tamanho = numero.innerText.length - 1;
     let ultimo_numero = numero.innerText[tamanho];
 
     if (isNaN(ultimo_numero) == true || isNaN(num.innerText) == true) {
-        numero.innerText = `${numero.innerText} ${num.innerText}`;
+        if (ultimo_numero == "√") {
+            if (num.innerText == "√") {
+                alert("Raiz Quadrada deve ser acompanhado com um numero");
+                return null;
+            } else {
+                numero.innerText += num.innerText;
+            }
+        } else {
+            numero.innerText = `${numero.innerText} ${num.innerText}`;
+        }
     } else {
         numero.innerText += num.innerText;
     }
 }
 
+function calculoTecla() {
+    let lista_calculo = numero.innerText.split(" ");
+    console.log(lista_calculo);
+    // let calculaT = eval(numero.innerText);
+    // numero.innerText = calculaT;
+    // console.log(calculaT);
+}
+
+function limpar() {
+    numero.innerText = ``;
+}
