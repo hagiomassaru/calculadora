e18e638a81323d1644745c1784ad59e0bb108f8c
Massaru <massaruhagiopro@gmail.com>
2022-01-01 01:00:13 +0900

    terminando logica

--- script.js
+++ script.js
@@ -21,11 +21,21 @@ function logicaDeTeclas(num) {
 }
 
 function calculoTecla() {
-    let lista_calculo = numero.innerText.split(" ");
-    console.log(lista_calculo);
-    // let calculaT = eval(numero.innerText);
-    // numero.innerText = calculaT;
-    // console.log(calculaT);
+    let lista_calculo = numero.innerText.split(" "); // separa a calculo que foi retirado na variavel numero que armazena o elemento html <h1 type="text" id="calculo"></h1>
+    let indice = -1; // essa variavel serve para podermos usar o indice do elemento que estamos trabalhando no for
+    for (var i of lista_calculo) {
+        // aqui vamos varrer cada elemento
+        indice++;
+        if (i.includes('√') == true){
+            let numero_raiz = i.split('');
+            // percebi que shift em strings nao funciona, entao precisei tranformar em array
+            let largura_elemento = i.length;
+            numero_raiz = parseInt(numero_raiz.splice(1,largura_elemento).toString().replace(/,/g,'')); // Nessa parte utilizo o splice ao inves do shift e retiro o simbolo √ e retorno como string como .toString() e retiro as virgulas gerada com replace()
+            numero_raiz = Math.sqrt(numero_raiz);
+            lista_calculo[indice/*como podemos ver como a variavel indice foi util */] = numero_raiz;
+        }
+    }
+    numero.innerText = eval(lista_calculo.toString().replace(/,/g,''));
 }
 
 function limpar() {
